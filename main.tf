37db9d3fa70abccf8ceb02cc788773933cbc6f86
timothy-spencer <timothy.spencer@gsa.gov>
2020-04-23 12:47:34 -0700

    use latest codebuild image, install yq and git

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_codebuild_project" "nessus" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:2.0"
+    image                       = "aws/codebuild/standard:4.0"
     type                        = "LINUX_CONTAINER"
     privileged_mode             = true
 
